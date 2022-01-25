094904c673644c77d3d9b5ae58c924385fdeee10
Thomas Ngo <thomasngo818@gmail.com>
2022-01-25 03:39:06 +0000

    Added /urls endpoint with initial code for using ejs template

--- express_server.js
+++ express_server.js
@@ -21,6 +21,13 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls", (req, res) => {
+  console.log("In callback for /urls endpoint")
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
