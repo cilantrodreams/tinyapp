dbd047f507f19cec519d05a3794ccfbe5ecc78aa
Thomas Ngo <thomasngo818@gmail.com>
2022-01-25 04:21:48 +0000

    Added header partial and updated all .ejs files to use it

--- express_server.js
+++ express_server.js
@@ -28,7 +28,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  console.log(urlDatabase[req.params.shortURL]);
+  console.log("In callback for /urls/:shortURL endpoint");
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
