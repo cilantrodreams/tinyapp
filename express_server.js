dcc30c439197e06aa342c18805afdc32ce7d60b0
Thomas Ngo <thomasngo818@gmail.com>
2022-01-25 05:31:53 +0000

    updated POST route so that it adds the inputted url to the database along with a newly generated string as its key

--- express_server.js
+++ express_server.js
@@ -45,8 +45,10 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  console.log(req.body);
+  urlDatabase[generateRandomString()] = req.body.longURL;
+  console.log(urlDatabase);
+  res.send("Ok");
 });
 
 app.listen(PORT, () => {
