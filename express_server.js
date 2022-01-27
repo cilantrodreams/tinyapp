f66bf6bef8daed19266854ffbcf5f6f87f84bee7
Thomas Ngo <thomasngo818@gmail.com>
2022-01-27 18:00:34 +0000

    Updated /urls/new route handler to check for login before they can visit the page

--- express_server.js
+++ express_server.js
@@ -77,10 +77,14 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = {
-    user: users[req.cookies["user_id"]],
-  };
-  res.render('urls_new', templateVars);
+  if (!req.cookies["user_id"]) {
+    res.redirect('/login');
+  } else {
+    const templateVars = {
+      user: users[req.cookies["user_id"]],
+    };
+    res.render('urls_new', templateVars);
+  }
 });
 
 app.get("/urls/:shortURL", (req, res) => {
