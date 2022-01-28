64047ef5082473806d35a4a4e4ce668849e130a2
Thomas Ngo <thomasngo818@gmail.com>
2022-01-28 02:38:15 +0000

    moved all current helper functions to be exported

--- helpers.js
+++ helpers.js
@@ -1,11 +1,30 @@
-const getUserByEmail = function(email, users) {
-  let user = undefined;
-  for (const data in users) {
-    if (email === users[data].email) {
-      user = users[data];
+const generateUserHelpers = (users, urlDatabase) => {
+
+  const getUserByEmail = function(email) {
+    let user = undefined;
+    for (const data in users) {
+      if (email === users[data].email) {
+        user = users[data];
+      }
     }
+    return user;
+  };
+
+  const generateRandomString = function() {
+    return Math.random().toString(36).slice(2, 8);
+  };
+
+  const urlsForUser = function(id) {
+    const userURLS = {};
+    for (const url in urlDatabase) {
+      if (urlDatabase[url].userID === id) {
+        userURLS[url] = urlDatabase[url];
+      }
+    }
+    return userURLS;
   }
-  return user;
+
+  return { getUserByEmail, generateRandomString, urlsForUser }
 };
 
-module.exports = { getUserByEmail };
+module.exports = generateUserHelpers;
