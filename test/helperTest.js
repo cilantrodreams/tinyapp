0c6a9fb1345b1819f5f6ca9df8b6e9f0796cbf72
Thomas Ngo <thomasngo818@gmail.com>
2022-01-28 03:10:25 +0000

    added unit tests for all helper functions

--- test/helperTest.js
+++ test/helperTest.js
@@ -1,6 +1,17 @@
 const { assert } = require('chai');
 
-const { getUserByEmail } = require('../helpers.js');
+const generateUserHelpers = require('../helpers.js');
+
+const testUrlDatabase = {
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "userRandomID"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "user2RandomID"
+  }
+};
 
 const testUsers = {
   "userRandomID": {
@@ -15,15 +26,47 @@ const testUsers = {
   }
 };
 
+const {
+  getUserByEmail,
+  generateRandomString,
+  urlsForUser
+} = generateUserHelpers(testUsers, testUrlDatabase);
+
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedUserID = "userRandomID";
     assert.strictEqual(user.id, expectedUserID);
   });
-  it('should return an empty object if user not found', function() {
+  it('should return undefined if user not found', function() {
     const user = getUserByEmail("notauser@example.com", testUsers)
-    const expectedUser = {};
+    const expectedUser = undefined;
     assert.deepEqual(user, expectedUser);
+  });
+});
+
+describe('generateRandomString', function() {
+  it('should return a string with 6 characters', function() {
+    const string = generateRandomString();
+    const expectedLength = 6;
+    assert.strictEqual(string.length, 6);
+  });
+});
+
+describe('urlsForUser', function() {
+  it('should return the urls associated with a user id', function() {
+    const urls = urlsForUser("user2RandomID");
+    const expectedUrls = {
+      "9sm5xK": {
+        longURL: "http://www.google.com",
+        userID: "user2RandomID"
+      }
+    };
+    assert.deepEqual(urls, expectedUrls);
+  });
+  it('should return an empty object if no urls found', function() {
+    const urls = urlsForUser("nonexistantID");
+    const expectedUrls = {};
+    assert.deepEqual(urls, expectedUrls);
   });
 });
