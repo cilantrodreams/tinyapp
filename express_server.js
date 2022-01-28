6ddba2ac4f694cc6de4e6516ea981a185968f7d8
Thomas Ngo <thomasngo818@gmail.com>
2022-01-28 03:19:53 +0000

    changed POST /logout route to redirect to root

--- express_server.js
+++ express_server.js
@@ -201,7 +201,7 @@ app.post('/login', (req, res) => {
 
 app.post('/logout', (req, res) => {
   req.session = null;
-  res.redirect('/urls');
+  res.redirect('/');
 });
 
 app.post('/register', (req, res) => {
